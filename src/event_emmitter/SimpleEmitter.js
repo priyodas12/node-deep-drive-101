af7cbec8363357f320a661ab7c3f3f7482f4e1bd
priyobrato.das <priyodas12@gmail.com>
2025-09-08 12:12:21 +0530

    event emitter

--- src/event_emmitter/SimpleEmitter.js
+++ src/event_emmitter/SimpleEmitter.js
@@ -5,6 +5,7 @@ import {v4 as uuidv4} from "uuid";
 //EventEmitter = Pub/Sub → all listeners on an event get notified.
 //It’s like a radio broadcast: one message, many listeners tuned to the same frequency.
 //It’s not a message queue where only one consumer gets the message.
+// visit: https://nodejs.org/docs/latest/api/events.html
 
 
 const emitter = new EventEmitter();
@@ -15,12 +16,22 @@ emitter.on("event-a", () => {
 
 emitter.on("event-b", () => {
     console.log("listener-2: consuming...", Date.now());
+
 });
 
-emitter.on("event-c", (msg) => {
-    console.log("listener-3: consuming...", msg);
+emitter.on("event-c", (a, b) => {
+    console.log("listener-3: consuming...", a, b);
+    console.log(a, b, this, this === emitter);
 });
 
+// async way
+emitter.on("event-cb", (x, y) => {
+    console.log(x, y);
+    setTimeout((a, b) => {
+        console.log("listener-4: consuming...", Date.now(), a, b);
+    }, 2000, x, y);
+});
 emitter.emit("event-a");
 emitter.emit("event-b");
-emitter.emit("event-c", uuidv4());
+emitter.emit("event-c", uuidv4(), uuidv4());
+emitter.emit("event-cb", uuidv4(), uuidv4());
